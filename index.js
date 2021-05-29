57964fad50c798243fe5fbc582a8facfe94c830a
Justin Johnson <jjustin634@gmail.com>
2021-05-29 10:19:26 -0500

    adding all code for admin text message along with a text to ensure service ran and the number of birthdays that were found.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 const User = require("./models/user");
+const Admin = require("./models/admin");
 
 // exclusing dotenv config from production
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
@@ -31,21 +32,70 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Schedule tasks to be run on the server.
-cron.schedule('00 08 * * *', function () {
-    getBirthdays();
-    sendTest();
+// cron.schedule('00 08 * * *', function () {
+//     getBirthdays();
+//     sendTest();
+// });
+
+cron.schedule('* * * * *', function () {
+    getBirthdays(); // fetch birthdays and send messages
+    testAdmin();    // fetch and text anybody if an admin message was created
 });
 
 async function getBirthdays() {
+    let birthdayCount = 0;
     User.find().then(user => {
         user.forEach((element) => {
-            findBirthdays(element);
+            findBirthdays(element).then((result) => {
+                if (result == 1) {
+                    birthdayCount++;
+                }
+            });
+        });
+    });
+
+    setTimeout(function () { sendTest(birthdayCount); }, 10000);
+}
+
+async function sendAdminText(messageBody, messageTo) {
+    // send text to Justin notfifying that an admin text was sent
+    new Promise(function (resolve, reject) {
+        client.messages
+            .create({
+                body: messageBody,
+                messagingServiceSid: 'MGd15a148e7bc6f6130e81dbccf13652b1',
+                to: messageTo
+            })
+            .then(message => console.log(message.sid))
+            .done();
+        resolve('Text was successfully sent')
+    });
+}
+
+async function testAdmin() {
+    Admin.find().then(adminn => {
+        adminn.forEach((user) => {
+            let today = new Date();
+            today = today.toString().slice(4, 10)
+
+            var mydate = new Date(user.birthdays);
+            var dateMatch = mydate.toDateString().includes(today);
+            if (dateMatch == true) {
+                // send the user the birthday message
+                sendAdminText(user.message, user.phone_number);
+                // send myself a text to know the admin message has been sent
+                let adminMessage = "Admin text message was sent to: " + user.name;
+                sendAdminText(adminMessage, "7153070876");
+            } else {
+                console.log('No admin message for today.')
+            }
         })
     });
 }
 
 async function findBirthdays(user) {
     let today = new Date();
+    let foundBirthday = 0;
     today = today.toString().slice(4, 10)
 
     new Promise(function (resolve, reject) {
@@ -53,6 +103,7 @@ async function findBirthdays(user) {
             let birthday = new Date(element.birthdayDate)
             birthday = birthday.toString().slice(4, 10)
             if (birthday == today) {
+                foundBirthday++;
                 let res = await sendText(element.birthdayName, user.name, user.phone_number);
                 resolve(res);
             } else {
@@ -60,6 +111,8 @@ async function findBirthdays(user) {
             }
         });
     });
+
+    return foundBirthday;
 }
 
 async function sendText(birthdayName, userName, userPhone) {
@@ -76,11 +129,11 @@ async function sendText(birthdayName, userName, userPhone) {
     });
 }
 
-async function sendTest() {
+async function sendTest(birthdayCount) {
     new Promise(function (resolve, reject) {
         client.messages
             .create({
-                body: "Good Morning JJ, the birthday app has started its run for the day.",
+                body: "Good Morning JJ, the birthday app has ran and " + birthdayCount + " birthdays were found.",
                 messagingServiceSid: 'MGd15a148e7bc6f6130e81dbccf13652b1',
                 to: '7153070876'
             })
